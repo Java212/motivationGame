ea90dd70759362972b7ed5511b705d107cd91123
Alex-Stronin <118293157+Alex-Stronin@users.noreply.github.com>
2023-07-24 20:24:02 +0300

    HT-8 method POST does not work

--- src/main/java/Notes/ControllerThymeleaf.java
+++ src/main/java/Notes/ControllerThymeleaf.java
@@ -2,6 +2,8 @@ package Notes;
 
 import Notes.model.Notes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,19 +18,16 @@ public class ControllerThymeleaf {
     public static List<Notes> notes = new ArrayList<>();
 
     @RequestMapping({"/"})
-    public String index() {
+        public String index() {
         return "index";
     }
 
-    @RequestMapping(path = "/today", method = RequestMethod.GET)
-    public ModelAndView getEventsForToday() {
-        ModelAndView mv = new ModelAndView("index");
-        mv.addObject("notes", notes);
-        return mv;
+    @RequestMapping(method = RequestMethod.GET)
+    public String viewNotes(Model model){
+        model.addAttribute("notes",notes);
+        return "notes";
     }
-
-
-    @PostMapping
+    @PostMapping ("/notesThymeleaf")
     public String addNote(String text){
         Notes note = new Notes();
         note.setText(text);
@@ -38,3 +37,10 @@ public class ControllerThymeleaf {
     }
 }
 
+//    public ModelAndView submitData(@ModelAttribute Notes note)
+//    {
+//        ModelAndView modelandview = new ModelAndView();
+//        modelandview.addObject("notes", notes);
+//        modelandview.setViewName("index");
+//        return modelandview;
+//    }
