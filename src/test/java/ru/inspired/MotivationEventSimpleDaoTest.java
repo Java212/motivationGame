c433178eecf3e298fe9f84e0f2744240f75b3ac1
BajanovVladimir <bajanov_v_s@mail.ru>
2023-08-01 16:30:17 +0400

    change MotivationEventSimpleDaoTest.java

--- src/test/java/ru/inspired/MotivationEventSimpleDaoTest.java
+++ src/test/java/ru/inspired/MotivationEventSimpleDaoTest.java
@@ -1,5 +1,4 @@
 package ru.inspired;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -14,20 +13,38 @@ import java.util.NoSuchElementException;
 public class MotivationEventSimpleDaoTest {
     @Test
     void test_that_the_class_was_created(){
-        MotivationEventSimpleDao motivationEvents = new MotivationEventSimpleDao();
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(new ClassPathResource("events-test.xml"));
+
+        List  <MotivationEvent> List = factory.getBean("events", ArrayList.class);
+
+        MotivationEventSimpleDao motivationEvents = new MotivationEventSimpleDao(List);
         List<MotivationEvent> eventList =  motivationEvents.getMotivationEvents();
-        Assertions.assertEquals(2,eventList.size());
+        Assertions.assertEquals(3,eventList.size());
     }
 
     @Test
     void test_that_getEventById_works(){
-        MotivationEventSimpleDao motivationEvents = new MotivationEventSimpleDao();
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(new ClassPathResource("events-test.xml"));
+
+        List  <MotivationEvent> List = factory.getBean("events", ArrayList.class);
+
+        MotivationEventSimpleDao motivationEvents = new MotivationEventSimpleDao(List);
         MotivationEvent event = motivationEvents.getEventById(1);
-        Assertions.assertEquals("сделать что-то",event.getDescription());
+        Assertions.assertEquals("Сделана утренняя зарядка",event.getDescription());
     }
     @Test
     void test_that_class_throws_an_exception_when_an_element_is_not_found_by_the_seam(){
-        MotivationEventSimpleDao motivationEvents = new MotivationEventSimpleDao();
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(new ClassPathResource("events-test.xml"));
+
+        List  <MotivationEvent> List = factory.getBean("events", ArrayList.class);
+
+        MotivationEventSimpleDao motivationEvents = new MotivationEventSimpleDao(List);
         Assertions.assertThrows(NoSuchElementException.class,()->motivationEvents.getEventById(7));
     }
 
